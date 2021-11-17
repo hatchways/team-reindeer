e9166452c9fc0a16b3c16aa5ea60fe5b7333b848
sholabioku <bilushonline@gmail.com>
2021-11-17 13:43:29 +0100

    Pretify code

--- server/test/auth.spec.js
+++ server/test/auth.spec.js
@@ -1,18 +1,18 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const { server } = require("../app.js");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { server } = require('../app.js');
 
 chai.should();
 chai.use(chaiHttp);
 
-describe("GET /auth/logout", () => {
-  it("should return 200 and a response message", (done) => {
+describe('GET /auth/logout', () => {
+  it('should return 200 and a response message', (done) => {
     chai
       .request(server)
       .get(`/auth/logout/`)
       .end((err, res) => {
         res.should.have.status(200);
-        res.text.should.equal("You have successfully logged out");
+        res.text.should.equal('You have successfully logged out');
         done();
       });
   });
