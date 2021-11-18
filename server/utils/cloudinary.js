b2d43f6191cc164f149d3e09f0601261bcf822ae
sholabioku <bilushonline@gmail.com>
2021-11-18 09:12:48 +0100

    Export cloudinary uploads method and its functionality

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -6,3 +6,21 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
+
+exports.uploads = (file, folder) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(
+      file,
+      (result) => {
+        resolve({
+          url: result.url,
+          id: result.public_id,
+        });
+      },
+      {
+        resource_type: 'auto',
+        folder,
+      }
+    );
+  });
+};
