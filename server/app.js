458bdc9f38e26c14dfa508b2e7e5dd3478278d38
sholabioku <bilushonline@gmail.com>
2021-11-18 14:28:27 +0100

    Use app.post()

--- server/app.js
+++ server/app.js
@@ -46,28 +46,22 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/upload-images', upload.array('image'), async (req, res) => {
+app.post('/upload-images', upload.array('image'), async (req, res) => {
   const uploader = async (path) => await cloudinary.uploads(path, 'Images');
 
-  if (req.method === 'POST') {
-    const urls = [];
-    const files = req.files;
-    for (const file of files) {
-      const { path } = file;
-      const newPath = await uploader(path);
-      urls.push(newPath);
-      fs.unlinkSync(path);
-    }
-
-    res.status(200).json({
-      message: 'images uploaded successfully',
-      data: urls,
-    });
-  } else {
-    res.status(405).json({
-      err: `${req.method} method not allowed`,
-    });
+  const urls = [];
+  const files = req.files;
+  for (const file of files) {
+    const { path } = file;
+    const newPath = await uploader(path);
+    urls.push(newPath);
+    fs.unlinkSync(path);
   }
+
+  res.status(200).json({
+    message: 'images uploaded successfully',
+    data: urls,
+  });
 });
 
 app.use('/auth', authRouter);
