c4551a2fc2152c65711b314201c5b16904bce329
sholabioku <bilushonline@gmail.com>
2021-11-18 08:56:05 +0100

    Add validation for file to upload functionality into utils/multer file

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -8,3 +8,11 @@ const storage = multer.diskStorage({
     cb(null, new Date().toISOString() + file.originalname);
   },
 });
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
