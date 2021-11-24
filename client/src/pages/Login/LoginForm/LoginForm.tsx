7e4ab1c68c4eeea402a179fddbb9be45312f6573
sholabioku <bilushonline@gmail.com>
2021-11-24 10:38:46 +0100

    Refracture the functionality

--- client/src/pages/Login/LoginForm/LoginForm.tsx
+++ client/src/pages/Login/LoginForm/LoginForm.tsx
@@ -35,6 +35,27 @@ export default function Login({ handleSubmit }: Props): JSX.Element {
   const { updateLoginContext } = useAuth();
   const { updateSnackBarMessage } = useSnackBar();
 
+  const handleClick = () => {
+    login('lukmanbioku@gmail.com', '1234567')
+      .then((data) => {
+        if (data.error) {
+          // setSubmitting(false);
+          updateSnackBarMessage(data.error.message);
+        } else if (data.success) {
+          updateLoginContext(data.success);
+        } else {
+          // should not get here from backend but this catch is for an unknown issue
+          console.error({ data });
+
+          // setSubmitting(false);
+          updateSnackBarMessage('An unexpected error occurred. Please try again');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Formik
       initialValues={{
@@ -103,24 +124,7 @@ export default function Login({ handleSubmit }: Props): JSX.Element {
               color="primary"
               className={classes.submit}
               onClick={() => {
-                login('lukmanbioku@gmail.com', '1234567')
-                  .then((data) => {
-                    if (data.error) {
-                      // setSubmitting(false);
-                      updateSnackBarMessage(data.error.message);
-                    } else if (data.success) {
-                      updateLoginContext(data.success);
-                    } else {
-                      // should not get here from backend but this catch is for an unknown issue
-                      console.error({ data });
-
-                      // setSubmitting(false);
-                      updateSnackBarMessage('An unexpected error occurred. Please try again');
-                    }
-                  })
-                  .catch((err) => {
-                    console.log(err);
-                  });
+                handleClick();
               }}
             >
               {isSubmitting ? <CircularProgress style={{ color: 'white' }} /> : 'Demo User'}
