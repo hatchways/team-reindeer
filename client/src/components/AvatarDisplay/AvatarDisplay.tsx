09f51460a2cd2954e3311ba3a7035b1fc94ea22e
Gagandeep <gagandeep.developer.95@gmail.com>
2021-11-27 11:26:17 -0500

    Modified some components to build UI

--- client/src/components/AvatarDisplay/AvatarDisplay.tsx
+++ client/src/components/AvatarDisplay/AvatarDisplay.tsx
@@ -1,13 +1,7 @@
 import Avatar from '@material-ui/core/Avatar';
-import { User } from '../../interface/User';
 
-interface Props {
-  loggedIn: boolean;
-  user: User;
-}
-
-const AvatarDisplay = ({ user }: Props): JSX.Element => {
-  return <Avatar alt="Profile Image" src={`https://robohash.org/${user.email}.png`} />;
+const AvatarDisplay = (): JSX.Element => {
+  return <Avatar alt="Profile Image" />;
 };
 
 export default AvatarDisplay;
--- client/src/components/Bookings/useStyles.ts
+++ client/src/components/Bookings/useStyles.ts
@@ -1,19 +1,30 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
-  wrapper: {
-    width: 350,
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 450,
+    padding: theme.spacing(10),
+    marginTop: theme.spacing(10),
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+  upcomingRoot: {
+    width: 450,
+    padding: theme.spacing(10),
+    marginTop: theme.spacing(10),
+    height: 300,
+    overflow: 'auto',
+  },
+  upcomings: {
+    padding: theme.spacing(10),
+    marginTop: theme.spacing(10),
   },
   title: {
     fontSize: 14,
   },
   pos: {
     marginBottom: 12,
+  },
+  clientName: {
+    marginLeft: theme.spacing(10),
   },
 }));
 
