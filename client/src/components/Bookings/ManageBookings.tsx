f9e0f6e9df1fe42944c5673465ecfa021e5adcb7
Gagandeep <gagandeep.developer.95@gmail.com>
2021-11-27 11:26:17 -0500

    Modified some components to build UI

--- client/src/components/Bookings/ManageBookings.tsx
+++ client/src/components/Bookings/ManageBookings.tsx
@@ -1,12 +1,27 @@
 import Box from '@material-ui/core/Box';
-import NextBooking from './NextBooking';
-import UpcomingBookings from './UpcomingBookings';
+import BookingCard from './BookingCard';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import useStyles from './useStyles';
 
 const ManageBookings = (): JSX.Element => {
+  const classes = useStyles();
   return (
-    <Box>
-      <NextBooking />
-      <UpcomingBookings />
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <BookingCard />
+      <Box>
+        <Card className={classes.upcomingRoot}>
+          <Typography className={classes.title} variant="h6" gutterBottom>
+            Upcoming Bookings:
+          </Typography>
+          <BookingCard />
+          <BookingCard />
+          <BookingCard />
+          <BookingCard />
+          <BookingCard />
+          <BookingCard />
+        </Card>
+      </Box>
     </Box>
   );
 };
--- client/src/pages/Bookings/useStyles.ts
+++ client/src/pages/Bookings/useStyles.ts
@@ -2,14 +2,14 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    border: '1px solid black',
+    // border: '1px solid black',
     marginTop: theme.spacing(50),
   },
   bookings: {
-    border: '1px solid black',
+    // border: '1px solid black',
   },
   bookingCalender: {
-    border: '1px solid black',
+    // border: '1px solid black',
   },
 }));
 
