c569e8a266fa17a78707620e95dcda57b15cef5c
sholabioku <bilushonline@gmail.com>
2021-11-28 15:48:56 +0100

    Add textfield for firstname and lastname

--- client/src/components/EditProfile/EditForm/EditForm.tsx
+++ client/src/components/EditProfile/EditForm/EditForm.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import useStyles from './useStyles';
-import { TextField, Button, Typography, Grid, Select, MenuItem } from '@material-ui/core';
-import { Formik, FormikHelpers } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
+import Textfield from '../../FormsUI/Textfield';
+import { Grid, makeStyles, Typography } from '@material-ui/core';
 
 const INITIAL_FORM_STATE = {
   firstName: '',
@@ -13,6 +12,14 @@ const INITIAL_FORM_STATE = {
 const FORM_VALIDATION = Yup.object().shape({
   firstName: Yup.string().required('This field cannot be empty'),
   lastName: Yup.string().required('This field cannot be empty'),
+});
+
+const useStyles = makeStyles({
+  label: {
+    fontSize: 19,
+    color: 'rgb(0,0,0,0.4)',
+    paddingLeft: '5px',
+  },
 });
 
 const EditForm = (): JSX.Element => {
@@ -29,73 +36,31 @@ const EditForm = (): JSX.Element => {
           console.log(values);
         }}
       >
-        {({ handleSubmit, handleChange, values, touched, errors, isSubmitting }) => (
-          <form onSubmit={handleSubmit} className={classes.form}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Typography variant="h6" align="center">
-                  Edit Profile
-                </Typography>
-              </Grid>
-              <Grid item xs={3} style={{ marginTop: '30px' }}>
-                <Typography variant="h6">First Name</Typography>
-              </Grid>
-              <Grid item xs={9}>
-                <TextField
-                  fullWidth
-                  className={classes.inputs}
-                  label={<Typography className={classes.label}>John</Typography>}
-                  type="text"
-                  name="firstName"
-                  helperText={touched.firstName ? errors.firstName : ''}
-                  error={touched.firstName && Boolean(errors.firstName)}
-                  value={values.firstName}
-                  onChange={handleChange}
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid item xs={3} style={{ marginTop: '30px' }}>
-                <Typography variant="h6">Last Name</Typography>
-              </Grid>
-              <Grid item xs={9}>
-                <TextField
-                  fullWidth
-                  className={classes.inputs}
-                  label={<Typography className={classes.label}>Doe</Typography>}
-                  type="text"
-                  name="lastName"
-                  value={values.lastName}
-                  onChange={handleChange}
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid item xs={3} style={{ marginTop: '30px' }}>
-                <Typography variant="h6">Gender</Typography>
-              </Grid>
-              <Select
-                style={{ flex: 1 }}
-                labelId="demo-customized-select-label"
-                id="demo-customized-select"
-                value={values.gender}
-                onChange={handleChange}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-              <Grid></Grid>
-
-              {/* <Box textAlign="center" className={classes.submitDiv}>
-              <Button type="submit" size="large" variant="contained" color="primary" className={classes.submit}>
-                save
-              </Button>
-            </Box> */}
+        <Form>
+          <Grid container alignItems="center" spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h6" align="center">
+                Edit Profile
+              </Typography>
             </Grid>
-          </form>
-        )}
+            <Grid item xs={12} sm={3}>
+              <Typography variant="body1" style={{ color: '#000' }}>
+                First Name
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <Textfield name="firstName" label={<Typography className={classes.label}>John</Typography>} />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Typography variant="body1" style={{ color: '#000' }}>
+                Last Name
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <Textfield name="lastName" label={<Typography className={classes.label}>Doe</Typography>} />
+            </Grid>
+          </Grid>
+        </Form>
       </Formik>
     </div>
   );
