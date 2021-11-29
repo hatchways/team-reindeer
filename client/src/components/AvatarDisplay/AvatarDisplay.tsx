7bd1bd8009a964dd9a8a51ec38617a4f2c6fad59
Gagandeep <gagandeep.developer.95@gmail.com>
2021-11-29 13:52:35 -0500

    adding optional props to AvatarDisplay component

--- client/src/components/AvatarDisplay/AvatarDisplay.tsx
+++ client/src/components/AvatarDisplay/AvatarDisplay.tsx
@@ -1,6 +1,12 @@
 import Avatar from '@material-ui/core/Avatar';
+import { User } from '../../interface/User';
 
-const AvatarDisplay = (): JSX.Element => {
+interface Props {
+  loggedIn?: boolean;
+  user?: User;
+}
+
+const AvatarDisplay = ({ user }: Props): JSX.Element => {
   return <Avatar alt="Profile Image" />;
 };
 
