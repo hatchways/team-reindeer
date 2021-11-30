82c177717c48e1c5d6423cf7539c27ad8509031b
Gagandeep <gagandeep.developer.95@gmail.com>
2021-11-30 10:21:02 -0500

    removed  unneccesory /create path

--- server/routes/request.js
+++ server/routes/request.js
@@ -9,7 +9,7 @@ const {
 } = require("../controllers/request");
 
 router.route("/").get(protect, getRequests);
-router.route("/create").post(protect, createRequest);
+router.route("/").post(protect, createRequest);
 router.route("/:id").patch(protect, updateRequest);
 
 module.exports = router;
