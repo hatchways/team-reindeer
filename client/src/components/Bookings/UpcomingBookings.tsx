d9cabb62ae88ecc498e3d9091b2d1a10e0e642c4
Gagandeep <gagandeep.developer.95@gmail.com>
2021-12-01 09:11:56 -0500

    Modified Booking Card based on context

--- client/src/components/Bookings/UpcomingBookings.tsx
+++ client/src/components/Bookings/UpcomingBookings.tsx
@@ -2,26 +2,23 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import Booking from './Booking';
 import useStyles from './useStyles';
-// This array is just for dummy text. It will get removed after integration.
-const bookings = [
-  { timing: '14 April 2021, 10-12 AM', clientName: 'Sakshi', status: 'accepted' },
-  { timing: '02 December 2021, 10-12 AM', clientName: 'Shubham', status: 'declined' },
-  { timing: '05 December 2021, 10-12 AM', clientName: 'Guru', status: 'pending' },
-];
+import { useRequest } from '../../context/useBookingContext';
 
 const UpcomingBookings = (): JSX.Element => {
   const classes = useStyles();
+  const request = useRequest();
+
   return (
     <Card className={classes.upcomingBookings}>
       <Typography className={classes.title} variant="h6" gutterBottom>
         Upcoming Bookings:
       </Typography>
-      {bookings.map((booking, i) => (
+      {request.bookings.map((booking, i) => (
         <Booking
           key={i}
           bookingType="upcomings"
-          timing={booking.timing}
-          clientName={booking.clientName}
+          start={booking.start}
+          sitterName={booking.sitterName}
           status={booking.status}
         />
       ))}
