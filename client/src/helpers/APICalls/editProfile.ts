ed4c1489c6c1dc2c0d64acf6fbae4ba892ed8db4
sholabioku <bilushonline@gmail.com>
2021-12-02 07:45:14 +0100

    Add editProfile helper function to make APICalls

---
+++ client/src/helpers/APICalls/editProfile.ts
@@ -0,0 +1,27 @@
+import { AuthApiData } from '../../interface/AuthApiData';
+import { FetchOptions } from '../../interface/FetchOptions';
+
+const editProfile = async (
+  firstName: string,
+  lastName: string,
+  description: string,
+  address: string,
+  phoneNumber: string,
+  dateOfBirth: Date,
+  availability: [Date],
+  photo: string,
+): Promise<AuthApiData> => {
+  const fetchOptions: FetchOptions = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ firstName, lastName, description, address, phoneNumber, dateOfBirth, availability, photo }),
+    credentials: 'include',
+  };
+  return await fetch(`/profile/edit`, fetchOptions)
+    .then((res) => res.json())
+    .catch(() => ({
+      error: { message: 'Unable to connect to server. Please try again' },
+    }));
+};
+
+export default editProfile;
