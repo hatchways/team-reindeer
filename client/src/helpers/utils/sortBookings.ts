cacb8f32a18c3a765d143ed1de9197eba04f8fff
Gagandeep <gagandeep.developer.95@gmail.com>
2021-12-11 12:12:01 -0500

    deleted unnecessary files

--- client/src/helpers/utils/sortBookings.ts
+++
@@ -1,5 +0,0 @@
-import { mockBookingData } from '../../pages/Bookings/mockBookingData';
-
-const sortedBookings = mockBookingData.sort((a, b) => a.start.getTime() - b.start.getTime());
-
-export default sortedBookings;
