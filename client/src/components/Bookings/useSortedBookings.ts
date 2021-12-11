aa643e038ea20effff19295fe78f0e6796b4faae
Gagandeep <gagandeep.developer.95@gmail.com>
2021-12-11 13:12:04 -0500

    added custom hook to sort the booking array

---
+++ client/src/components/Bookings/useSortedBookings.ts
@@ -0,0 +1,12 @@
+import { useRequest } from '../../context/useBookingContext';
+import { Booking } from '../../interface/Booking';
+
+const useSortedBookings = (): Booking[] => {
+  const { bookings } = useRequest();
+  const sortedBookings = bookings.sort((a: Booking, b: Booking) => {
+    return new Date(a.duration.start).getTime() - new Date(b.duration.start).getTime();
+  });
+  return sortedBookings;
+};
+
+export default useSortedBookings;
