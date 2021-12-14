6c2857ff0aaec574ad1bea40ed30bcee7bc93633
sholabioku <bilushonline@gmail.com>
2021-12-14 21:33:38 +0100

    A fix

--- client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
+++ client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, ImgHTMLAttributes, ReactNode, useRef, useState } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { Box } from '@material-ui/core';
-import { Form, Formik } from 'formik';
+import { Avatar, Box } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,36 +16,48 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const UploadPhoto = (): JSX.Element => {
-  const [fileInputState, setFileInputState] = useState('');
+const UploadPhoto: React.FC = (): JSX.Element => {
+  const inputFile = useRef<HTMLInputElement>(null);
+  const [fileInputState, setFileInputState] = useState<File | undefined>(undefined);
+
+  const [previewSource, setPreviewSource] = useState<string | undefined>(undefined);
+
   const classes = useStyles();
 
-  const handleChange = (e: any) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    if (!(e.target.files instanceof FileList)) return;
+
     const file = e.target.files[0];
+
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      if (!reader.result) {
+        setFileInputState(file);
+      }
+    };
   };
 
   return (
     <Box className={classes.root}>
-      <Formik initialValues={{ file: '' }} onSubmit={(values) => console.log(values)}>
-        {() => (
-          <Form>
-            <input
-              accept="image/*"
-              className={classes.input}
-              id="contained-button-file"
-              multiple
-              type="file"
-              value={fileInputState}
-              onChange={handleChange}
-            />
-            <label htmlFor="contained-button-file">
-              <Button variant="contained" size="large" color="primary" component="span">
-                Upload
-              </Button>
-            </label>
-          </Form>
-        )}
-      </Formik>
+      <form>
+        <input
+          ref={inputFile}
+          accept="image/*"
+          className={classes.input}
+          id="contained-button-file"
+          multiple
+          type="file"
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="contained-button-file">
+          <Button variant="contained" size="large" color="primary" component="span">
+            Upload
+          </Button>
+        </label>
+      </form>
+      {previewSource && <Avatar src={previewSource} alt="upload photo" />}
     </Box>
   );
 };
