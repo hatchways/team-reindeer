269450a66dd7c4ff8f4d4a309c68dbfb115e3ef5
sholabioku <bilushonline@gmail.com>
2021-12-15 15:55:55 +0100

    Add UploadPhoto functional and its functionality

--- client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
+++ client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, ImgHTMLAttributes, ReactNode, useRef, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Avatar, Box } from '@material-ui/core';
@@ -13,51 +13,47 @@ const useStyles = makeStyles((theme: Theme) =>
     input: {
       display: 'none',
     },
+    avatar: {
+      width: '300px',
+      height: '300px',
+    },
+    button: {
+      marginLeft: '100px',
+      marginTop: '10px',
+    },
   }),
 );
 
 const UploadPhoto: React.FC = (): JSX.Element => {
-  const inputFile = useRef<HTMLInputElement>(null);
-  const [fileInputState, setFileInputState] = useState<File | undefined>(undefined);
-
-  const [previewSource, setPreviewSource] = useState<string | undefined>(undefined);
-
   const classes = useStyles();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const reader = new FileReader();
-    if (!(e.target.files instanceof FileList)) return;
+  const [selectedImage, setSelectedImage] = useState<File>();
 
-    const file = e.target.files[0];
-
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      if (!reader.result) {
-        setFileInputState(file);
-      }
-    };
+  const imageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedImage(e.target.files[0]);
+    }
   };
 
   return (
     <Box className={classes.root}>
       <form>
         <input
-          ref={inputFile}
           accept="image/*"
           className={classes.input}
           id="contained-button-file"
           multiple
           type="file"
-          onChange={handleChange}
+          onChange={imageChange}
           required
         />
         <label htmlFor="contained-button-file">
-          <Button variant="contained" size="large" color="primary" component="span">
+          {selectedImage && <Avatar src={URL.createObjectURL(selectedImage)} alt="Thumb" className={classes.avatar} />}
+          <Button variant="contained" size="large" color="primary" component="span" className={classes.button}>
             Upload
           </Button>
         </label>
       </form>
-      {previewSource && <Avatar src={previewSource} alt="upload photo" />}
     </Box>
   );
 };
