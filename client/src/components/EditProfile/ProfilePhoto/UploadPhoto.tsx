a9e7f31243d759adbea49a32376ae09300e2e239
sholabioku <bilushonline@gmail.com>
2021-12-15 22:28:14 +0100

    Add deleteIcon, and clean codes

--- client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
+++ client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
@@ -1,34 +1,8 @@
 import React, { ChangeEvent, useState } from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { Avatar, Box, Typography } from '@material-ui/core';
+import { Avatar, Box, IconButton, Typography, Button } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    input: {
-      display: 'none',
-    },
-    avatar: {
-      width: '300px',
-      height: '320px',
-    },
-    button: {
-      marginLeft: '30px',
-      marginTop: '20px',
-    },
-    typography: {
-      marginBottom: '30px',
-    },
-  }),
-);
+import useStyles from './useStyles';
 
 const UploadPhoto: React.FC = (): JSX.Element => {
   const classes = useStyles();
@@ -56,15 +30,24 @@ const UploadPhoto: React.FC = (): JSX.Element => {
           onChange={imageChange}
           required
         />
-        <label htmlFor="contained-button-file">
+        <label htmlFor="outlined-button-file">
           {selectedImage ? (
             <Avatar src={URL.createObjectURL(selectedImage)} alt="Thumb" className={classes.avatar} />
           ) : (
             <Avatar className={classes.avatar} />
           )}
+          <Typography variant="subtitle1" className={classes.typography}>
+            Be sure to use a photo that clearly show your face
+          </Typography>
           <Button variant="outlined" size="large" color="primary" component="span" className={classes.button}>
             Upload a file from your device
           </Button>
+          <Box>
+            <IconButton aria-label="delete" className={classes.iconButton}>
+              <DeleteIcon fontSize="large" />
+              <Typography variant="subtitle1">Delete photo</Typography>
+            </IconButton>
+          </Box>
         </label>
       </form>
     </Box>
