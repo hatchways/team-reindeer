17407f7113ccecb24dfea7de84bdf52eba64ce33
sholabioku <bilushonline@gmail.com>
2021-12-15 21:59:52 +0100

    Add avarta for , typography and style them

--- client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
+++ client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useState } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { Avatar, Box } from '@material-ui/core';
+import { Avatar, Box, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,8 +21,11 @@ const useStyles = makeStyles((theme: Theme) =>
       height: '320px',
     },
     button: {
-      marginLeft: '100px',
-      marginTop: '10px',
+      marginLeft: '30px',
+      marginTop: '20px',
+    },
+    typography: {
+      marginBottom: '30px',
     },
   }),
 );
@@ -41,6 +44,9 @@ const UploadPhoto: React.FC = (): JSX.Element => {
   return (
     <Box className={classes.root}>
       <form>
+        <Typography variant="h5" className={classes.typography}>
+          Profile Photo
+        </Typography>
         <input
           accept="image/*"
           className={classes.input}
@@ -51,9 +57,13 @@ const UploadPhoto: React.FC = (): JSX.Element => {
           required
         />
         <label htmlFor="contained-button-file">
-          {selectedImage && <Avatar src={URL.createObjectURL(selectedImage)} alt="Thumb" className={classes.avatar} />}
-          <Button variant="contained" size="large" color="primary" component="span" className={classes.button}>
-            Upload
+          {selectedImage ? (
+            <Avatar src={URL.createObjectURL(selectedImage)} alt="Thumb" className={classes.avatar} />
+          ) : (
+            <Avatar className={classes.avatar} />
+          )}
+          <Button variant="outlined" size="large" color="primary" component="span" className={classes.button}>
+            Upload a file from your device
           </Button>
         </label>
       </form>
