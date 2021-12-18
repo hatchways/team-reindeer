dc05c24b1f127cc8a8b88f7b221b2965560c4a49
Gagandeep <gagandeep.developer.95@gmail.com>
2021-12-18 12:31:11 -0500

    Worked on suggested changes

--- client/src/components/Dashboard/Listing/useStyles.ts
+++ client/src/components/Dashboard/Listing/useStyles.ts
@@ -3,19 +3,21 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   profile: {
     margin: theme.spacing(10),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
   profileInfo1: {
     marginTop: theme.spacing(5),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    minHeight: '250px',
-  },
-  icon: {
-    color: '#f14140',
+    minHeight: '200px',
+    textAlign: 'center',
   },
   profileInfo2: {
     display: 'flex',
+    alignItems: 'center',
     justifyContent: 'space-between',
   },
 }));
