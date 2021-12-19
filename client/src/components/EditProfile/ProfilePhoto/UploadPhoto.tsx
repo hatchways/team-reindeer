c18f63dc824c131a491c0643824205f520b60938
sholabioku <bilushonline@gmail.com>
2021-12-19 09:56:35 +0100

    Remove unused

--- client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
+++ client/src/components/EditProfile/ProfilePhoto/UploadPhoto.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, MouseEvent, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Avatar, Box, Typography, Button } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 
