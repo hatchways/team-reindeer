e8f486d13983462b00ed2a540907efa1cb47f9f3
sholabioku <bilushonline@gmail.com>
2022-01-04 15:02:21 +0100

    Add schema of message which has sender, content, conversation,and readBy fields

---
+++ server/models/Message.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      trim: true,
+    },
+    conversation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+    },
+    readBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = Message = mongoose.model("Message", messageSchema);
