618fc326d62f8fdc2a2ce5781bc166979726420c
sholabioku <bilushonline@gmail.com>
2022-01-04 15:34:20 +0100

    Add a functionality to check if conversation exist between the users , and populate the conversation with users details, latestMessage and sender details(name and email

--- server/controllers/conversation.js
+++ server/controllers/conversation.js
@@ -7,8 +7,24 @@ const asyncHandler = require("express-async-handler");
 //@access Private
 exports.createConversation = asyncHandler(async (req, res, next) => {
   const { userId } = req.body;
+
   if (!userId) {
     res.status(400);
     throw new Error("UserId param not sent with request");
   }
+
+  let isConversation = await Conversation.find({
+    isGroupConversation: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  isConversation = await User.populate(isConversation, {
+    path: "latestMessage.sender",
+    select: "name  email",
+  });
 });
